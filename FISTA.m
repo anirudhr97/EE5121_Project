6b14b1916b694fe6f281c03c3809bd0df1574231
Anirudh R <r.anirudhsrinivas@gmail.com>
2021-11-29 18:57:56 +0530

    Added x and z History outputting

--- FISTA.m
+++ FISTA.m
@@ -1,4 +1,4 @@
-function [x,func_eval,numMV] = FISTA(A,b,tau,Gamma,max_iter,type)
+function [x,func_eval,numMV,xHist,zHist] = FISTA(A,b,tau,Gamma,max_iter,type)
 %{
 Function to implement the Fast Iterative Shrinkage Thresholding Algorithm
 for 3 cases:    Quadratic       + L1 regularization
@@ -7,33 +7,46 @@ for 3 cases:    Quadratic       + L1 regularization
 %}
 x = zeros(size(A,2),1);
 numMV = max_iter;
+func_eval = zeros(max_iter+1, 1);
+xHist = zeros(max_iter+1, 1);
+zHist = zeros(max_iter+1, 1);
+xHist(1) = x;
+zHist(1) = x;
 mu = 0;
 z = x;
 L = norm(A,'fro')^2;
 if strcmp(type,'quad_l1')
+    func_eval(1) = 0.5*x'*A*x - b'*x + tau*norm(x,1);
     for i = 1 : max_iter
         mu = 0.5*(1 + sqrt(1 + 4*mu^2)); gamma = (1-mu)/(mu+1);
         zprev = z;
         z = soft_thresholding((x - (A*x -b)/L), tau/L);
         x = (1-gamma)*z + gamma*zprev;
-        func_eval(i) = 0.5*x'*A*x - b'*x + tau*norm(x,1);
+        func_eval(i+1) = 0.5*x'*A*x - b'*x + tau*norm(x,1);
+        xHist(i+1) = x;
+        zHist(i+1) = z;
     end
 elseif strcmp(type,'ls_l1')
+    func_eval(1) = norm(A*x - b,2)^2  + tau*norm(x,1);
     for i = 1 : max_iter
         mu = 0.5*(1 + sqrt(1 + 4*mu^2)); gamma = (1-mu)/(mu+1);
         zprev = z;
         z = soft_thresholding((x - (A'*(A*x -b))/L), tau/L);
         x = (1-gamma)*z + gamma*zprev;
-        func_eval(i) = norm(A*x - b,2)^2  + tau*norm(x,1) ;
+        func_eval(i+1) = norm(A*x - b,2)^2  + tau*norm(x,1);
+        xHist(i+1) = x;
+        zHist(i+1) = z;
     end
 elseif strcmp(type,'reg_quad_l1')
+    func_eval(1) = 0.5*x'*A*x - b'*x + Gamma*norm(x,2) + tau*norm(x,1);
     for i = 1 : max_iter
         mu = 0.5*(1 + sqrt(1 + 4*mu^2)); gamma = (1-mu)/(mu+1);
         zprev = z;
         z = soft_thresholding((x - ((A+2*gamma*eye(size(A,1)))*x -b)/L), tau/L);
         x = (1-gamma)*z + gamma*zprev;
-        func_eval(i) = 0.5*x'*A*x - b'*x + Gamma*norm(x,2) + tau*norm(x,1) ;
+        func_eval(i+1) = 0.5*x'*A*x - b'*x + Gamma*norm(x,2) + tau*norm(x,1);
+        xHist(i+1) = x;
+        zHist(i+1) = z;
     end
 end
-    
 end
